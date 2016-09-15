3839154c83cd0570c0a129c26eb35ad46943f55f
xieheng <xieheng@DevilFactory.com>
2016-09-15 10:55:43 +0800

    add space controllers

---
+++ Core/Public/Space/UFixedSpace.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// 
+/// </summary>
+public class UFixedSpace : UControl
+{
+    #region Data
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private int _pixel = 3;
+
+    #endregion
+
+    #region Construction
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="pixel"></param>
+    public UFixedSpace(int pixel)
+    {
+        _pixel = pixel;
+    }
+
+    #endregion
+
+    #region Override
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public override void OnGUI()
+    {
+        GUILayout.Space(_pixel);
+    }
+
+    #endregion
+}
